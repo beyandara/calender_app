2139e7c7a7011d511b6c3284bd13cf63bfb76c4c
Therese IV <theresebvik@gmail.com>
2024-01-31 11:00:35 +0100

    testing

--- app/src/main/java/com/example/calenderapp/MainActivity.kt
+++ app/src/main/java/com/example/calenderapp/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CalenderAppTheme {
-        Greeting("Android")
+        Greeting("Test")
     }
 }
