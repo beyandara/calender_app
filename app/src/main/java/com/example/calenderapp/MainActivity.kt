59aaaba1e29ab0df3e313808d9d007d6703969b5
Therese IV <theresebvik@gmail.com>
2024-02-02 11:36:27 +0100

    made some minor adjustments to the CalenderLayout function

--- app/src/main/java/com/example/calenderapp/MainActivity.kt
+++ app/src/main/java/com/example/calenderapp/MainActivity.kt
@@ -4,12 +4,18 @@ import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -51,13 +58,16 @@ fun CalendarLayout(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .height(40.dp)
         )
-        Text(
-            text = showMonth(monthNumber = 2),
-            fontWeight = FontWeight.Bold,
-            color = Color.Green,
-            modifier = modifier
-                .align(Alignment.Start)
-        )
+        Row (
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ){
+            Text(
+                text = showMonth(monthNumber = 2),
+                fontWeight = FontWeight.Bold,
+                color = Color.Green,
+            )
+        }
         Box(
             modifier = Modifier
         ) {
@@ -65,7 +75,26 @@ fun CalendarLayout(modifier: Modifier = Modifier) {
                 painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = null,
                 modifier = Modifier
-                    .fillMaxSize()
+                    .size(400.dp)
+
+            )
+            Row (
+                modifier = Modifier
+                    .size(width = 400.dp, height = 36.dp)
+                    .border(BorderStroke(2.dp, Color.Black))
+            ){
+                Text(
+                    text = "Row test",
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Row (
+            modifier = Modifier
+                .border(BorderStroke(2.dp, Color.Black))
+        ){
+            Text(
+                text = stringResource(R.string.bottom_click_text)
             )
         }
     }
