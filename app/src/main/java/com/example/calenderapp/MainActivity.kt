937bdda20bbffbb26ba51cff673068408b984eb6
Therese IV <theresebvik@gmail.com>
2024-02-02 10:15:29 +0100

    added composable function showMonth and CalendarLayout

--- app/src/main/java/com/example/calenderapp/MainActivity.kt
+++ app/src/main/java/com/example/calenderapp/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.calenderapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calenderapp.ui.theme.CalenderAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("tester")
+                    CalendarLayout()
                 }
             }
         }
@@ -30,17 +42,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalendarLayout(modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Spacer(
+            modifier = Modifier
+                .height(40.dp)
+        )
+        Text(
+            text = showMonth(monthNumber = 2),
+            fontWeight = FontWeight.Bold,
+            color = Color.Green,
+            modifier = modifier
+                .align(Alignment.Start)
+        )
+        Box(
+            modifier = Modifier
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun showMonth(monthNumber: Int): String {
+    val numberToMonthName = when (monthNumber) {
+        1 -> stringResource(R.string.month_1)
+        2 -> stringResource(R.string.month_2)
+        3 -> stringResource(R.string.month_3)
+        4 -> stringResource(R.string.month_4)
+        5 -> stringResource(R.string.month_5)
+        6 -> stringResource(R.string.month_6)
+        7 -> stringResource(R.string.month_7)
+        8 -> stringResource(R.string.month_8)
+        9 -> stringResource(R.string.month_9)
+        10 -> stringResource(R.string.month_10)
+        11 -> stringResource(R.string.month_11)
+        else -> stringResource(R.string.month_12)
+    }
+    return numberToMonthName
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CalenderAppTheme {
-        Greeting("Test")
+        CalendarLayout()
     }
 }
