a5bbaf661b809c50ce945fc7f053c5205bd32b80
beyandara <beyan97@hotmail.com>
2024-02-07 10:40:46 +0100

    deleted lines

--- app/src/androidTest/java/com/example/calenderapp/CalenderTest.kt
+++ app/src/androidTest/java/com/example/calenderapp/CalenderTest.kt
@@ -1,9 +1,6 @@
 package com.example.calenderapp
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
 
